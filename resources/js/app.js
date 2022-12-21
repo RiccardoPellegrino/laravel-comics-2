74fd5aa11e9271efee06b1af7933c58602106495
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-12-21 21:54:02 +0100

    bozza js sicuro di voler cancellare

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,31 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+let modalColl = document.getElementsByClassName("myModal");
+
+// Get the button that opens the modal
+let btnColl = document.getElementsByClassName("myBtn");
+
+// Get the <span> element that closes the modal
+let spanColl = document.getElementsByClassName("close");
+for(let i=0;i<btnColl.length;i++){
+
+// When the user clicks the button, open the modal 
+btnColl[i].onclick = function(e) {
+    e.preventDefault();
+    modalColl[i].style.display = "block";
+}
+spanColl[i].onclick = function() {
+    modalColl[i].style.display = "none";
+}
+
+
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalColl) {
+    modalColl.style.display = "none";
+  }
+}
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -11,7 +11,6 @@
             <div class="mio_container">
                 @foreach ($comics as $comic)
                     <div class="card" style="width: 12rem;">
-
                         <a href="{{ route('comics.show', $comic->id) }}"><img src="{{ $comic['thumb'] }}"
                                 class="card-img-top" alt="..."></a>
                         <div class="card-body">
@@ -21,13 +20,42 @@
                             <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-secondary">Dettagli</a>
                             <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-success ms-2">Modifica</a>
                         </div>
-                        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <div class="bg-dark">
-                                <button type="submit" class="btn btn-danger w-100">Cancella</button>
+
+
+                        <div class="bg-dark">
+                            <button type="submit" class="btn btn-danger w-100 myBtn">Cancella</button>
+                        </div>
+
+
+                        {{-- <div class="modal myModal">
+                            <div class="modal-content">
+                                <span class="close my-close">chiuditi</span>
+                                <h4 class="text-center mb-4">Sei sicuro di voler cancellare questo Comic?</h4>
+                                <form action="{{route('comics.destroy', $comic->id)}}" method="POST" class="text-center">
+                                    @csrf
+                                    @method('DELETE')
+                                    <div>
+                                    <button type="submit" class="my-btn me-4">Si</button>
+                                    <span class="my-btn no" id="btn-close">No</span>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div> --}}
+                        <div id="" class="modal myModal">
+                            <!-- Modal content -->
+                            <div class="modal-content">
+                                <span class="close btn btn-secondary">CHIUDITI</span>
+                                <br>
+                                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <div class="text-center">
+                                        <span class="title-delete">Sei sicuro di eliminare {{ $comic->title }}</span>
+                                        <button type="submit" class='btncancella mt-3 btn btn-danger'>si</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 @endforeach
             </div>
