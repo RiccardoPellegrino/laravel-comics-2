2894b8502f597be76473e019247776e8fab3bc33
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-12-21 12:13:52 +0100

    modifiche create e show

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,58 +1,57 @@
 @extends('layouts.app')
 @section('content')
-<section class="container">
+<section class="w-75 m-auto text-center py-5">
     <form action="{{ route('comics.store') }}" method="POST" class="text-white">
         @csrf
-
-            <div>
-                <label for="title">Titolo</label>
-                <input type="text" name="title" id="title">
-            </div>
-
-            <div>
-                <label for="description">Descrizione</label>
-                <input type="text" name="description" id="description">
-            </div>
-
-            <div>
-                <label for="thumb">Thumb</label>
-                <input type="text" name="thumb" id="thumb">
-            </div>
-
-            <div>
-                <label for="price">Prezzo</label>
-                <input type="text" name="price" id="price">
-            </div>
-
-            <div>
-                <label for="series">Serie</label>
-                <input type="text" name="series" id="series">
-            </div>
-
-            <div>
-                <label for="sale_date">Data di uscita</label>
-                <input type="text" name="sale_date" id="sale_date">
-            </div>
-
-            <div>
-                <label for="type">Tipo</label>
-                <input type="text" name="type" id="type">
-            </div>
-
-            <div>
-                <label for="artists">Artista</label>
-                <input type="text" name="artists" id="artists">
-            </div>
-
-            <div>
-                <label for="writers">Scrittore</label>
-                <input type="text" name="writers" id="writers">
-            </div>
-
-            <div>
-                <input type="submit" value="Invia">
-            </div>
-        </form>
      
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="title">Titolo</label>
+            <input type="text" name="title" id="title" placeholder="Inserisci un titolo" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="description">Descrizione</label>
+            <input type="text" name="description" id="description" placeholder="Inserisci una descrizione" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="thumb">Thumb</label>
+            <input type="text" name="thumb" id="thumb" placeholder="Inserisci un'immagine" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="price">Prezzo</label>
+            <input type="text" name="price" id="price" placeholder="Inserisci un prezzo" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="series">Serie</label>
+            <input type="text" name="series" id="series"  placeholder="Inserisci la serie" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="sale_date">Data di uscita</label>
+            <input type="text" name="sale_date" id="sale_date" placeholder="aaaa-mm-gg" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="type">Tipo</label>
+            <input type="text" name="type" id="type"  placeholder="Inserisci il tipo" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-2">
+            <label for="artists">Artista</label>
+            <input type="text" name="artists" id="artists"  placeholder="Inserisci gli artisti" class="w-25 p-1">
+        </div>
+
+        <div class="d-flex flex-column align-items-center mb-4">
+            <label for="writers">Scrittore</label>
+            <input type="text" name="writers" id="writers" placeholder="Inserisci gli scrittori" class="w-25 p-1">
+        </div>
+
+        <div>
+            <input type="submit" value="Invia" class="btn btn-secondary">
+        </div>
+     </form>
 </section>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts.app')
 @section('content')
-  <section class="container">
-   <h1 class="text-white">{{$comic->title}}</h1>
-   <p class="text-white">{{$comic->description}}</p>
-    <img src="{{$comic->thumb}}" alt="">
-   <p class="text-white">{{$comic->prince}}</p>
-   <p class="text-white">{{$comic->series}}</p>
-   <p class="text-white">{{$comic->sale_date}}</p>
-   <p class="text-white">{{$comic->type}}</p>
-   
-  </section>
+    <section class="text-white p-5">
+        <h1>{{$comic->title}}</h1>
+        <p>{{$comic->description}}</p>
+        <img src={{$comic->thumb}} alt="">
+        <div>
+            <span>{{$comic->price}}</span>
+            <br>
+            <span>{{$comic->series}}</span>
+            <br>
+            <span>{{$comic->sale_date}}</span>
+            <br>
+            <span>{{$comic->type}}</span>
+        </div>
+    </section>
+    <div class="d-flex justify-content-center align-content-center">
+        <div class="load_button">
+            <span><a href="{{ route('comics.index') }}" class="text-white text-uppercase text-decoration-none">back</a></span>
+        </div>
+    </div> 
 @endsection
